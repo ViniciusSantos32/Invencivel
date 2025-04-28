80567ff99516b2428b375c0b3ef5ad2a348c45ef
felipe-baierski <felipe_b_vieira@estudante.sesisenai.org.br>
2025-04-28 08:33:38 -0300

    Criação do código, ou também importação

---
+++ invencivelemtfodasimfdsovinicius/android/app/src/main/kotlin/com/example/invencivelemtfodasimfdsovinicius/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.invencivelemtfodasimfdsovinicius
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ invencivelemtfodasimfdsovinicius/lib/Splash/Splash_Screen.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient:
+        
+         LinearGradient(begin: Alignment.centerLeft, end: Alignment.centerRight, colors: [
+        Colors.black,
+        Color.fromARGB(255, 0, 44, 241),
+
+      ]),
+      ),
+      child: 
+  Padding(padding: EdgeInsets.only(top: 350), child:  Column(
+            
+            children: [
+              Text("PIUI TRAIN", style: GoogleFonts.russoOne(textStyle: TextStyle(color: const Color.fromARGB(255, 0, 44, 241),decoration: TextDecoration.none, fontWeight: FontWeight.bold)) ),
+                    ],
+                      ),)
+         
+          
+    );
+  }
+}
