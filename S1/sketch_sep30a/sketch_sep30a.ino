ef403e244982ff31d877f1f075e31937a40fc4f5
Gustavo H Engelmann <gustavo_engelmann@estudante.sesisenai.org.br>
2025-10-01 08:29:04 -0300

    adição do código do john programacion, adição de conexão com a internet

--- S1/sketch_sep30a/sketch_sep30a.ino
+++ S1/sketch_sep30a/sketch_sep30a.ino
@@ -1,6 +1,16 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <WiFi.h>
+WiFiClient client; //nome do cliente wifi
+const String SSID = "nome da rede"; //nome da rede wifi
+const String PASS = "senha da rede";//senha da rede
 
+void setup() {
+  Serial.begin(115200);
+  client.begin(home, senha);
+  Serial.println("Conectando no WiFi");
+  while(client.status()!= WL_CONNECTED){
+     Serial.print(".");
+     delay(200);
+  }
 }
 
 void loop() {
